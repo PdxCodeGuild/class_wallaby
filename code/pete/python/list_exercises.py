3503efbc8dde1c7d316005a5cfcbbc922572c482
Pete Jones <56445854+pjz987@users.noreply.github.com>
2021-07-01 13:41:54 -0700

    working on exercises

---
+++ code/pete/python/list_exercises.py
@@ -0,0 +1,59 @@
+"""
+Access element from list: mylist[i]
+Given the list of seasons, print out 'summer'
+"""
+
+seasons = ['spring', 'summer', 'fall', 'winter']
+
+# print(seasons)
+
+"""
+Access element from nested lists: mylist[i][j]
+Given the 2D list of seasons, print out 'july'
+"""
+
+spring = ['march', 'april', 'may']
+summer = ['june', 'july', 'august']
+fall = ['september', 'october', 'november']
+winter = ['december', 'january', 'febuary']
+
+seasons2d = [spring, summer, fall, winter]
+
+# print(seasons2d)
+
+"""
+Assignment mylist[i] = value
+In the list seasons, change 'fall' to 'autumn'
+"""
+
+# print(seasons)
+
+"""
+Assignment to nested lists
+Fix the 'febuary' typo in winter THROUGH seasons2d
+"""
+
+# print(seasons2d)
+
+"""
+Loops
+Loop over seasons to print out each season
+"""
+
+"""
+Loop over seasons to print out each season using range() and len()
+"""
+
+"""
+Nested Loops
+Loop over seasons2d AND the nested lists to print out each month
+"""
+
+"""
+Using range() and len(), loop over seasons2d to print out each month
+"""
+
+"""
+Inclusion: element in mylist
+"""
+
