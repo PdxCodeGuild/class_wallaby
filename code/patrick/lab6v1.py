5fe238b560c8fd678a1505fe8afd64ff6a3208b5
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-07-06 23:22:59 -0700

    completed lab6v1

--- code/patrick/lab6v1.py
+++ code/patrick/lab6v1.py
@@ -11,7 +11,17 @@ for i in range(len(number_2)):
         final.append(number_2[i])
 
     elif i % 2 == 0:
-        x = number_2[i] * 2  
+        x = number_2[i] * 2
+        if x > 9:
+            x = x - 9  
         final.append(x)
-print(final) 
-print(number_2)
+
+sum = 0
+for i in final:
+    sum += i
+
+ones_digit = sum%10
+if ones_digit == check_digit:
+    print("valid")
+else:
+    print('try again')
