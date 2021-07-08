cebf663bbb4d902dc93c4bceca6222b2ec0bb151
ccloter <ccloter@gmail.com>
2021-07-07 21:24:33 -0700

    'Lab5

--- code/carson/lab5.py
+++ code/carson/lab5.py
@@ -4,25 +4,40 @@ import random
 ticket = []
 
 winner = []
+i = 1
+while i <= 100000: #Loop function
+    
+    
+    
+    for i in range(0,6):
+        n = random.randint(1,99)
+        winner.append(n)
 
-for i in range(0,6):
-    n = random.randint(1,99)
-    winner.append(n)
+    for i in range(0,6):
+        n = random.randint(1,99)
+        ticket.append(n)
 
-for i in range(0,6):
-    n = random.randint(1,99)
-    ticket.append(n)
 
-'''total = winner - ticket''' # compare both list and return matches 
+total = set(winner) & set(ticket) # compare both list and return matches 
+
+
 
 payout = {
-'ticket' : 2,
- '1' : 4,
- '2' : 7,
- '3' : 100,
- '4' : 50000,
- '5' : 1000000,
- '6' : 25000000,
+ 1 : 4,
+ 2 : 7,
+ 3 : 100,
+ 4 : 50000,
+ 5 : 1000000,
+ 6 : 25000000
 }
 
-#print(total) #Print matches from total list
+x = len(total)
+
+x = payout.get(x)
+
+print(f' Congratulations you won ${x} !! :)' )
+
+print(f'Ticket Numbers: {ticket}')
+
+print(f'Winning Numbers: {winner}')
+#print(f'You Won ${x2} ')
