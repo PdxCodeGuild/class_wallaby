ef3879431394773570527519eea1f4872944d94b
Will-max-bit <whermann4@gmail.com>
2021-07-07 21:28:39 -0700

    fixed typo

--- code/will/for_loop.py
+++ code/will/for_loop.py
@@ -3,14 +3,14 @@
 '''
 in this for loop we are iterating over the index's 
 (the numerical position of an index in a list)
-by interating this way, we can extract the index and/or the value from a list
+by interating this way, we can extract the index and/or the element from a list
 '''
 fruits =['apples', 'bananas', 'pears', 'pineapples', 'beets', 'bears', 'battlestar', 'galactica']
 for i in range(len(fruits)):
         print(fruits[i], i)
         # print(i)
 # expected output
-#indice      index
+#element      index
 '''
 apples      0
 bananas     1
@@ -21,8 +21,8 @@ bears       5
 battlestar  6
 galactica   7
 '''
-# in the below for loop, we are simply iterating over the indice's in the list and printing
-# We do not get the index by doing a for loop this, they are handy when you only care about the indice,not the index
+# in the below for loop, we are simply iterating over the elements's in the list and printing
+# We do not get the index by doing a for loop this, they are handy when you only care about the element,not the index
 nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 for num in nums:
     print(num)
