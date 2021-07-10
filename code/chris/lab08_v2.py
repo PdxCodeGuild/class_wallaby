c1fac15e6c061b190ac00db95e2c299384d19c6f
ChrisHolley <asiholley@gmail.com>
2021-07-09 21:45:40 -0700

    lab08_v2 mid build of graph matrix

--- code/chris/lab08_v2.py
+++ code/chris/lab08_v2.py
@@ -1,13 +1,14 @@
+import copy
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(data):
   # This was a practice on what I could do with list comprehension. I want to eventually refactor this to be more reader friendly
   peak_list = [
-                idx for idx, value in enumerate(data)
-                  if ((0 if idx == 0 else data[idx - 1]) < value)
-                    and (0 if idx == len(data) - 1
-                    else data[idx + 1] < value)
+    idx for idx, value in enumerate(data)
+      if ((0 if idx == 0 else data[idx - 1]) < value)
+        and (0 if idx == len(data) - 1
+        else data[idx + 1] < value)
               ]
 
   return peak_list
@@ -28,7 +29,28 @@ def peaks_and_valleys(data):
   result_list.sort()
   return result_list
 
+decon_data = copy.copy(data)
+print(decon_data)
 
-print('peaks list:', peaks(data))
-print('valleys list:', valleys(data))
-print('peaks & valleys', peaks_and_valleys(data))
+while_tracker = True
+
+while while_tracker:
+  for jdx in range(len(decon_data)):
+    decon_data[jdx] -= 1
+  print(decon_data)
+
+
+graph_matrix_row = [
+  'X' for idx, value in enumerate(data)
+]
+
+graph = '  '.join(graph_matrix_row)
+
+# print(f'''
+
+#        {graph}
+# Data: {data}
+# Peaks: {peaks(data)}
+# Valleys: {valleys(data)}
+# ''')
+
