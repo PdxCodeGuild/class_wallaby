39bf7e05d7a98a75d9c1f9980bf8a4794d0c4b74
ChrisHolley <asiholley@gmail.com>
2021-07-09 20:58:09 -0700

    lab08_v1 valleys function

--- code/chris/lab08_v1.py
+++ code/chris/lab08_v1.py
@@ -2,9 +2,26 @@
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 def peaks(data):
-  peak_list = [idx for idx, value in enumerate(data) if ((0 if idx == 0 else data[idx - 1]) < value) and (0 if idx == len(data) - 1 else data[idx + 1] < value)]
-  # peak_list = [idx for idx, value in enumerate(data) if ((data[idx-1] or IndexError) < value) and ((data[idx+1] or IndexError) < value)]
+  # This was a practice on what I could do with list comprehension. I want to eventually refactor this to be more reader friendly
+  peak_list = [
+                idx for idx, value in enumerate(data)
+                  if ((0 if idx == 0 else data[idx - 1]) < value)
+                    and (0 if idx == len(data) - 1
+                    else data[idx + 1] < value)
+              ]
 
   return peak_list
 
-print(peaks(data))
+def valleys(data):
+  valleys_list = []
+  for idx, value in enumerate(data):
+    if idx > 0 and idx < len(data):
+      print('code runs ', idx)
+      if data[idx - 1] > value and data[idx + 1] > value:
+        valleys_list.append(idx)
+
+  return valleys_list
+
+
+print('peaks list:', peaks(data))
+print('valleys list:', valleys(data))
