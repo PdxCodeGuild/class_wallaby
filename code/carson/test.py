313026ee6c075e62a5124e61456f7c175fbd2749
ccloter <ccloter@gmail.com>
2021-07-09 21:32:04 -0700

    'lab5

--- code/carson/test.py
+++ code/carson/test.py
@@ -1,3 +1,21 @@
 students = ('julio' ,'megan' , 'carson')
 for student in students:
     print(student.capitalize())
+
+from random import choice 
+from colorama import init, Fore
+
+init()
+
+colors = [
+Fore.BLACK,
+Fore.RED,
+Fore.GREEN,
+Fore.YELLOW,
+Fore.BLUE
+    
+]
+def random_color():
+    return choice (colors)
+
+print(random_color())
