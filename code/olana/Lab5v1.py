9614706cde4855f8cde1e7b3394f999cdf7e33f9
ojwesley <olana.wesley@outlook.com>
2021-07-10 00:43:58 -0400

    Lab5

--- code/olana/Lab5v1.py
+++ code/olana/Lab5v1.py
@@ -7,12 +7,14 @@
 #6. Add to your balance the winnings from your matches
 #7. After the loop, print the final balance
 
-
+#Citations:
 #https://pynative.com/python-random-randrange/
 #https://www.kite.com/python/docs/collections.Counter
-
-
-
+#https://www.w3schools.com/python/default.asp
+#https://stackoverflow.com/questions/21771964/list-index-comparison-in-python
+#https://thispointer.com/python-how-to-find-all-indexes-of-an-item-in-a-list/
+#https://www.codespeedy.com/python-check-if-all-the-elements-in-a-list-are-equal/
+#https://stackoverflow.com/questions/38035317/comparing-same-index-in-2-lists
 
 
 #1. Generate a list of 6 random numbers representing the winning ticket
@@ -25,10 +27,7 @@ def pick6():
 computer_ticket = pick6()
 a = enumerate(computer_ticket)
 print(list(a))
-#print(winning_ticket)
-
-#3. Generate a list of 6 random numbers representing the ticket
-#print("My Ticket: ", random.sample(range(0, 99), 6))
+#print(computer_ticket)
 
 def lottery_ticket():
   return(random.sample(range(0, 99), 6))  
@@ -37,13 +36,58 @@ b = enumerate(my_ticket)
 print(list(b))
 #print(my_ticket)
 
+winning_ticket = [True if i == j else False for i,j in zip(my_ticket, computer_ticket)]
+print(winning_ticket)
 
-#def num_matches():
-    #return 
+'''
+winning_ticket = []
+counter = 0
 
-if a == b:
-    print("match")
-else:
-    print("no match")
+for num in my_ticket:  
+    if num in computer_ticket:
+        counter +=1
+print(str(counter) + " matched")
+'''
+
+
+
+
+
+
+
+        
+
+
+#computer_ticket,my_ticket = [],[]
+#print[index for index,(e1,e2) in enumerate(zip(computer_ticket, my_ticket)) if e1 == e2]
+
+
+
+#for i, nums in enumerate(computer_ticket):
+    #print(f"{i}: {computer_ticket}")
+
+
+
+    
+#print(computer_ticket)
+#a = enumerate(computer_ticket)
+#print(list(a))
+#print(winning_ticket)
+
+#3. Generate a list of 6 random numbers representing the ticket
+#print("My Ticket: ", random.sample(range(0, 99), 6))
+'''
+def lottery_ticket():
+  return(random.sample(range(0, 99), 6))  
+my_ticket = lottery_ticket()
+print(my_ticket)
+b = enumerate(my_ticket)
+print(list(b))
+print(my_ticket)
+'''
+
+
+
+    
 
 
