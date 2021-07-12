f32f983547db406be0c98b68f29c497d12d93af1
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-07-11 22:13:08 -0700

    in progress, lab9v1

---
+++ code/patrick/lab10v1.py
@@ -0,0 +1,12 @@
+with open('lab10_list.csv') as file:
+    lines = file.read().split('\n')
+dict_1 = {}   
+names = ('')
+for i in range(len(lines)):
+    keys = (lines[0].split(','))
+for i in range(len(lines)-1):
+    values = lines[i+1]
+    print(values)
+    for i in range(1,1,1):
+        print(i)
+        
