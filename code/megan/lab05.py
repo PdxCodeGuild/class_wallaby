e01263250b4baf379c62f5afad3d911c82d89935
meganxcook <cook.me.eliz@gmail.com>
2021-07-12 20:03:56 -0700

    Lab 06 complete

--- code/megan/lab05.py
+++ code/megan/lab05.py
@@ -16,6 +16,12 @@ def pick6(list):
         pick_6_list.append(random.randint(1,99))
     return pick_6_list
 
+def winning_ticket():
+    winning_nums = []
+    while len(winning_nums) < 6:
+        winning_nums.append(random.randint(1,99))
+    return winning_nums
+print(winning_ticket())
 #print(f'{pick6(pick_6_list)}')
 
 def num_matches(winning, ticket):
@@ -27,20 +33,21 @@ winning = pick6(pick_6_list)
 # Start your balance at 0
 balance = 0
 
-# Loop 100,000 times, for each loop:
-for i in range(1, 100000):
-    # Generate a list of 6 random numbers representing the ticket
-    ticket = pick6(pick_6_list)
-    # Subtract 2 from your balance (you bought a ticket)
-    balance =- 2
-    # Find how many numbers match
-    matches = num_matches(winning, ticket)
-    # Add to your balance the winnings from your matches
-    if matches > 0:
-        balance += winnings_dict[matches]
+
+# # Loop 100,000 times, for each loop:
+# for i in range(1, 100000):
+#     # Generate a list of 6 random numbers representing the ticket
+#     ticket = pick6(pick_6_list)
+#     # Subtract 2 from your balance (you bought a ticket)
+#     balance =- 2
+#     # Find how many numbers match
+#     matches = num_matches(winning, ticket)
+#     # Add to your balance the winnings from your matches
+#     if matches > 0:
+#         balance += winnings_dict[matches]
     
-# After the loop, print the final balance
-print(balance)
+# # After the loop, print the final balance
+# print(balance)
 
 
 
--- code/megan/lab06.py
+++ code/megan/lab06.py
@@ -4,57 +4,48 @@ test_card_num = '4556737586899855'
 # 1. Convert the input string into a list of ints
 test_card_num_list = list(test_card_num)
 test_card_num_list = [int(i) for i in test_card_num_list]
-
-print(test_card_num_list)
-
-#for i in range(len(test_card_num_list)):
-#    print(test_card_num_list[i]) 
+print(f"1. {test_card_num_list}")
 
 # 2. Slice off the last digit. That is the check digit.
-sliced_list = []
-test_card_num_list = test_card_num_list[:-1]
-
-print(test_card_num_list)
+# test_card_num_list = test_card_num_list[:-1]
+check_digit = test_card_num_list.pop(-1)
+print(f"2. {test_card_num_list}")
 
 # 3. Reverse the digits.
-
-reversed_list = []
 test_card_num_list.reverse()
-
-print(test_card_num_list)
+print(f"3. {test_card_num_list}")
 
 # 4. Double every other element in the reversed list.
-    #slicing?
-
-print(reversed_list[::1*2]) # error
-
-#for i in range(reversed_list): # error
- #   if i % 2 == 0:
-  #      reversed_list = reversed_list * 2
-
-#print(test_card_num_list)
+every_other_list = test_card_num_list[:]
+every_other_list[::2] = [x*2 for x in every_other_list[::2]]
+print(f"4. {every_other_list}")
 
 # 5. Subtract nine from numbers over nine.
+subtract_9_list = []
+
+for i in range(len(every_other_list)):
+    if every_other_list[i] > 9:
+        subtract_9_list.append(every_other_list[i] - 9)
+    else:
+        subtract_9_list.append(every_other_list[i])
+
+print(f"5. {subtract_9_list}")
 
 # 6. Sum all values.
+sum = sum(subtract_9_list)
+
+print(f"6. {sum}")
 
 # 7. Take the second digit of that sum.
 
+# ones_digit = user_num % 10
+
+second_digit = sum % 10
+print(second_digit)
+
 # 8. If that matches the check digit, the whole card number is valid.
 
-
-"""
-# Double every other element in the reversed list.
-
-    # use enumerate?
-    # use slice?
-    double_every_other_list = reversed_list[::2]
-    
-
-
-if True:
-    print('Valid')
-
-    else:
-        print('Invalid')
-"""
+if second_digit == check_digit:
+    print("Valid")
+else:
+    print("Invalid")
