76cd8be62f3d46466c076e2cdf918d0efd95d449
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-07-13 18:29:30 -0700

    finished lab 11v2

--- code/patrick/lab11v2.py
+++ code/patrick/lab11v2.py
@@ -1,16 +1,28 @@
 lst_1 = [1,2,3,4,5,6,7,8,9,10]
-low = lst_1[0]
-high = lst_1[-1]
-def search_lst(list, object):
-    if low < high:
-        mid = high/2
-        if mid == object:
-            return list.index(object)
+x = len(lst_1)
+tgt_num = int(input('Enter the number you wish to find the index of: '))
+def search_lst(list, object, x):
+    low = lst_1[0]
+    high = lst_1[x-1]
+    mid = ((low + high) // 2)
+    while low <= high:
+        if object > high or object < low:
+            return 'Target out of range!' 
+        elif mid == object:
+            return list.index(mid)
+        elif object == high:
+            return list.index(high)
+        elif object == low:
+            return list.index(low)
         elif mid < object:
-            mid_1 = mid = high/2
-            for i in mid_1:
-                return list.index(object)
+            low = mid+1
+            continue
+        elif mid > object:
+            high = mid-1
+            continue
+        else:
+            return None
 
-print(search_lst(lst_1, 8))
+print(f'The target number of {tgt_num} has an index of {search_lst(lst_1, tgt_num, x)} in the current list.')
 
         
