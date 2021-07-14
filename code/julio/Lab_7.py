143d5171dae588f5f59841a171ca0270f4ad50e4
JM85305 <30480728+JM85305@users.noreply.github.com>
2021-07-13 18:30:32 -0700

    Lab 7 completed. Continuing lab 8

--- code/julio/Lab_7.py
+++ code/julio/Lab_7.py
@@ -1,11 +1,6 @@
+#------------------------------------------------------------------------#   
 def rot13(txt):
-    """Applies the rot13 cipher to a text. 
-    In the case of a plain text this will result in a cipher text
-    being preoduced. Ad a cipher text will be deciphered.
-    
-    args: txt : a text on which rot13 will be applied
-    returns: encrypted/decrypted version of the text"""
-
+#------------------------------------------------------------------------#   
     result = []
     for letter in txt:
         value = ord(letter)
@@ -13,22 +8,27 @@ def rot13(txt):
             value += 13
             if value > ord('Z'):
                 value = (value - ord('Z')) + ord('A') - 1
-                
-    
+ #------------------------------------------------------------------------#               
         elif value >= ord('a') and value <= ord('z'):
             value += 13
             if value > ord('z'):
                 value = (value - ord('z')) + ord('a') - 1
-
-        result.append( chr(value) )
-
+        result.append(chr(value))
     return "".join(result)
-
-
+#------------------------------------------------------------------------#
+print("#-------------------------------------------------------------#")
 while True:
-    msg = input("please type a message to encrypt or decrypt").strip()
+   
+    msg = input("please type a message to encrypt or decrypt: ").strip()
+    
     if not msg:
+        print("#-------------------------------------------------------------#")
         break
+#------------------------------------------------------------------------#
     enc = rot13(msg)
-    print(enc)
-    print(rot13(enc))
+    print("#-------------------------------------------------------------#")
+    print("1:",enc)
+    print("#-------------------------------------------------------------#")
+    print("2:",rot13(enc))
+    print("#-------------------------------------------------------------#")
+#------------------------------------------------------------------------#   
