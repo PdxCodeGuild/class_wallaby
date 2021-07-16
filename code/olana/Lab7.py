51b8cbc50963a219d5ce84fe2a0b3ad74a43a5b7
ojwesley <olana.wesley@outlook.com>
2021-07-16 16:45:29 -0400

    Lab 7 Final Update

--- code/olana/Lab7.py
+++ code/olana/Lab7.py
@@ -1,4 +1,29 @@
-index = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
 
-english = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', w, x, y, z]
-rot_13 =  ['n', 'o', p, q, r, s, t, u, v, w, x, y, z, a, b, c, d, e, f, g, h, i, j, k, l, m]
+#Cite: http://econowmics.com/python-rot-13-encryption-method/
+# Rot 13
+import string
+
+# The alphabet
+
+english_alphabet = string.ascii_lowercase
+
+# Function to encrypt a text based on the Rot-13 method
+def encrypt(text):
+    """The function receives a string as input, and encrypts it using the Rot-13 method """
+    
+    # Transform the string to lower-case chars
+    text = text.lower()
+    
+    # the encrypted message
+    rot_13_message = ''
+    
+    # Replace each letter in the string with a letter which is 13 positions further
+    for character in text:
+        if character.isalpha:
+            rot_13_message += english_alphabet[(english_alphabet.index(character) + 13) % 26]
+        else:
+            rot_13_message += character
+    return rot_13_message
+
+print(encrypt(input(" "))) 
+  
