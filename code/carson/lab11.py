0d3f45b0aec2edb19a4569e87fe6d24c7ae1469d
ccloter <ccloter@gmail.com>
2021-07-21 21:33:54 -0700

    'lab12

---
+++ code/carson/lab11.py
@@ -0,0 +1,9 @@
+def linear_search(nums, value):
+
+#'''loops through the given list and check if each element is equal to the value'''
+
+...
+# index 0  1  2  3  4  5  6  7
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+index = linear_search(nums, 3)
+print(index) # 2
