9b5ab32819b270320780cadee5aeeae1e2e14184
ChrisHolley <asiholley@gmail.com>
2021-07-22 18:35:15 -0700

    lab11 part 2: binary search complete

--- code/chris/lab11_v1.py
+++ code/chris/lab11_v1.py
@@ -1,3 +1,6 @@
+example_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+
+
 def linear_search(nums, value):
   counter = 0
   reader = nums[counter]
@@ -8,7 +11,22 @@ def linear_search(nums, value):
     reader = nums[counter]
   return counter
 
+# print(linear_search(example_1, 9))
 
-example_1 = [1, 2, 3, 4, 5, 6, 7, 8]
 
-print(linear_search(example_1, 9))
+
+def binary_search(nums, value):
+  low = 0
+  high = len(nums)
+  while low < high:
+    mid = (low + high) // 2
+    if nums[mid] < value:
+      low = mid + 1
+    elif nums[mid] > value:
+      high = mid - 1
+    else:
+      return mid
+  return 'target not found'
+
+# print(binary_search(example_1, 99))
+
