fb85b0b54c0ec401655a8cabd1556d0ffe017e9c
ChrisHolley <asiholley@gmail.com>
2021-07-22 18:50:07 -0700

    lab 11 part3: bubble sort complete

--- code/chris/lab11_v1.py
+++ code/chris/lab11_v1.py
@@ -1,4 +1,5 @@
 example_1 = [1, 2, 3, 4, 5, 6, 7, 8]
+unsorted_list = [6, 2, 5, 1, 9, 7, 8, 3, 4]
 
 
 def linear_search(nums, value):
@@ -12,8 +13,6 @@ def linear_search(nums, value):
   return counter
 
 # print(linear_search(example_1, 9))
-
-
 
 def binary_search(nums, value):
   low = 0
@@ -29,4 +28,20 @@ def binary_search(nums, value):
   return 'target not found'
 
 # print(binary_search(example_1, 99))
+
+def bubble_sort(nums):
+  change_flag = True
+  while change_flag == True:
+    change_flag = False
+    for idx, value in enumerate(nums):
+      if idx >= len(nums) - 1:
+        break
+      if value > nums[idx + 1]:
+        temp = value
+        nums[idx] = nums[idx + 1]
+        nums[idx + 1] = temp
+        change_flag = True
+  return nums
+
+# print(bubble_sort(unsorted_list))
 
