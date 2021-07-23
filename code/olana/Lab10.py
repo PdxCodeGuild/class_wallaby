dcf3424905256b4f05de517063a74bacf9d267d7
ojwesley <olana.wesley@outlook.com>
2021-07-23 00:38:11 -0400

    Lab 10 in progress

--- code/olana/Lab10.py
+++ code/olana/Lab10.py
@@ -1,28 +1,40 @@
-
-with open('dogs.csv', 'r') as file:
+'''
+showdogs_file = open('dogs.csv','r')
+for row in showdogs_file:
+    print(row.split(',')[0])
+'''
+with open('dogs.csv','r', encoding ='utf-8') as file:
     dogs = file.read().split('\n')
-    #print(dogs)
+    print(dogs)
 
 #hint: look up dictionary methods, create a line item
 
-dogs.append('labrador,large,sporting')
+dogs.append('labrador, medium, sporting')
 print(dogs)
 
+
 #Retrieve a record: ask the user for the breed of dog 
-search = input("Enter breed name: ")
+#search = input("Enter breed name: ")
 #find the breed in the list, and display its information
 
-if search == 'standard poodle':
+#results = dogs.get()
+#print(results)
+
+'''
+if search == 'border collie':
     print(dogs[1])
 elif search == 'pug':
     print(dogs[2])
-elif search == 'great dane':
+elif search == 'greyhound':
     print(dogs[3])
-elif search == 'border collie':
+elif search == 'poodle':
     print(dogs[4])
-elif search == 'labrador':
+elif search == 'norwich terrier':
+    print(dogs[5])
+elif search == 'akita':
     print(dogs[6])
-
+elif search == 'labrador':
+    print(dogs[7])
 #Ask the user to update the list
 
 update_breed = input("To update the dog's info type enter the breed: ")
@@ -30,8 +42,21 @@ update_attributes = input("Which attribute would you like to update, type size o
 if update_attributes == 'size':
     update_size_attributes = input("Update size: ")
     dogs.append(update_size_attributes)
+    print(dogs)
 else:
     update_group_attributes = input("Update group: ")
+    dogs.append(update_group_attributes)
 
+'''
+'''
+#cite: list_exercises.py
+spring = ['march', 'april', 'may']
+summer = ['june', 'july', 'august']
+fall = ['september', 'october', 'november']
+winter = ['december', 'january', 'febuary']
 
+seasons2d = [spring, summer, fall, winter]
 
+print(seasons2d[1][1])
+
+'''
