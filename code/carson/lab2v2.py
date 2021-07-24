55dd66a39027844439059d5c56ccb269ca400a07
ccloter <ccloter@gmail.com>
2021-07-23 23:40:24 -0700

    'lab13

--- code/carson/lab2v2.py
+++ code/carson/lab2v2.py
@@ -1,1 +1,18 @@
-hundreds digit = test_number % 100 // 10 
+nums = []
+
+while True:
+    user_input = input("Enter a number or enter 'done' to quit: ")
+    if user_input == 'done':
+       break    
+    user = int(user_input)
+    nums.append(user)
+    total = sum(nums)
+    print(total /len(nums))
+    
+   
+
+
+
+
+
+#hundreds_digit = test_number % 100 // 10 
