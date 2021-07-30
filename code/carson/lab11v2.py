06b7f76220487c3bf62031bb151ecd88592031dc
ccloter <ccloter@gmail.com>
2021-07-29 21:48:53 -0700

    'Lab11,14,15

--- code/carson/lab11v2.py
+++ code/carson/lab11v2.py
@@ -1,23 +1,46 @@
 nums = [1,2,3,4,5,6,7,8]
-n = len(nums)
+#n = len(nums)
 search = int(input('Enter a number : '))
 
-def binary_search(list, object, n):
-    low = nums[0]
-    high = nums[n-1]
-    mid = ((low + high) // 2)
-    while low <= high:
-        if object > high or object < low:
-            return 'Number out of range' 
-        elif mid == object:
-            return list.index(mid)
-        elif object == high:
-            return list.index(high)
-        elif object == low:
-            return list.index(low)
+# def binary_search(list, object, n):
+#     low = 0
+#     high =  n - 1
+#     mid = ((low + high) // 2)
+#     while low <= high:
+#         if object > high or object < low:
+#             return 'Number out of range' 
+#         elif mid == object:
+#             return list.index(mid)
+#         elif object == high:
+#             return list.index(high)
+#         elif object == low:
+#             return list.index(low)
+#         else:
+#             return 'Unable to find index'
+
+# print(f'Input Number: {search} \n Index: {binary_search(nums, search, n)} ')
+
+# '''2,5,6,7 don't work properly'''
+
+
+       
+            
+def binary_search(nums,i):
+    first = 0
+    print('first', first)
+    last = len(nums)-1
+    while  first <= last :
+        mid = (first + last) // 2
+        print('mid', mid)
+        guess = nums[mid]
+        print('guess',guess)
+        if guess == i :
+            return i
+        elif i < guess:
+            last = mid - 1
+            print('now last is ', last)
         else:
-            return 'Unable to find index'
+            first = mid + 1	
+    return None
 
-print(f'Input Number: {search} \n Index: {binary_search(nums, search, n)} ')
-
-'''2,3,5,6,7 don't work properly'''
+print(binary_search(nums, search))
--- code/carson/weatherlab.py
+++ code/carson/weatherlab.py
@@ -1,26 +1,47 @@
 """ have user pick from list of infomation to display
 
-i.e daily, weekly, and monthly forcast
+i.e current, weekly, and monthly forcast
 
 """
 
-
-
-
-choices = [1,2,3]
 import requests
+
 print(' Search for a city to get the weather ! :)')
 state = input('State: ')
-city = input(" City:  ")
-print(city)
-response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},{state}&units=imperial&appid=b55c99ab16c500de4bf79dd8c45ef232')
+city = input(' City:  ')
+choices = [1,2,3]
 
+response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city},{state}&units=imperial&appid=b55c99ab16c500de4bf79dd8c45ef232')
 data = response.json()
-print(data)
-temp = data['main']['temp']
-des = data['weather'][0]['description']
+
+lon = data['coord']['lon']
+lat = data['coord']['lat']
+
+
+response2 = requests.get(f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&units=imperial&appid=b55c99ab16c500de4bf79dd8c45ef232')
+data2 = response2.json()
+
+print(data2)
+
+
+while True:
+    choice =input(f'what data would you like to see? \n1 - Current Weather \n2 - Hourly Forcast \n3 - Forcast for 7 days \nInput: ')
+    choice = int(choice)
+    
+    if choice == 1:
+        temp = data['main']['temp']
+        des = data['weather'][0]['description']
+        loc = data['name']
+        print(f' The weather in {loc} is {temp} degrees with {des} ')
+        break
+
+    #if choice == 2:
+
+
+
+
 #print(des)
-loc = data['name']
+        
 #print(loc)
-print(f' The weather in {loc} is {temp} degrees with {des} ')
-                                   
+        
+                    
