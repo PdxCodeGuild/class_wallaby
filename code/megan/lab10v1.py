145ee76fd406f74ca49d41cbecb613a4f3864d70
meganxcook <cook.me.eliz@gmail.com>
2021-08-02 21:48:26 -0700

    lab10v1 complete

--- code/megan/lab10v1.py
+++ code/megan/lab10v1.py
@@ -1,111 +1,20 @@
-from csv import DictWriter
-
 with open('contacts.csv', 'r') as f:
     lines = f.read().split('\n')
     # print(lines)
-    for row in lines:
-        rows = row.split(',')
-        # print(rows)
 
-# headers = keys
-# rows = values
-
-# keys = {}
-contacts_dct = {}   
-values = {}
+contacts_dct = {}
+fields = []
 contacts = []
-
-# make first row (headers) the keys
-for i in range(len(rows)):
-    if i == 0:
-        contacts_dct.update({'first name': rows[0]})
-    elif i == 1:
-        contacts_dct.update({'last name': rows[1]})
-    elif i == 2:
-        contacts_dct.update({'city': rows[2]})
-    elif i == 3:
-        contacts_dct.update({'occupation': rows[3]})
-# print(keys)
-
-# add all values to list
-for row in rows:
-    contacts.append(contacts_dct)
-
-print(contacts)
-
-
-
-
-
-
-
-'''
-# zip keys and values dictionaries?
-
-
-keys = (list(contacts)) # ['name', 'lastname', 'city', 'occupation']
-print(keys)
-
-values = []
-
-
-
-values = (list())
-print(values)
 
 for i in range(len(lines)):
     values = lines[i]
-    keys.append(values)
+    fields.append(values)
+    # print(fields)
 
-contacts_lst = [] 
+for row in lines:
+    rows = row.split(',')
+    # print(rows)
+    contacts_dct = {'first name': rows[0], 'last name': rows[1], 'city': rows[2], 'occupation': rows[3]}
+    contacts.append(contacts_dct)
 
-contacts.append(contacts_dct)
-
-# append or zip?
-
-# zipped = zip(numbers, letters)
-
-
-
-
-dict.update({'key': values[i]})
-
-
-    
-    # contacts_list = []
-
-contacts = dict(zip(contacts))
-
-
-
-
-for contact in contacts_dct:
-    contact.append(contacts_dct)
-
-print(contacts_dct)
- 
-# turn into a list of dictionaries
-
-# zip + list (or dictionary?) comprehension
-
-
-
-# dictionary conversions
-# dict comprehensions
-
-# print(dict(contacts))
-
-# contacts = [dict()]
-# contacts = []
-
-# contacts = [
-#     {'first name': '', 'last name': '', 'city': '', 'occupation': ''},
-#     {'name': '', 'city': '', 'occupation': ''},
-#     {'name': '', 'city': '', 'occupation': ''},
-#     {'name': '', 'city': '', 'occupation': ''},
-# ]
-
-# Version 2: CRUD REPL
-
-# Version 3: (work backwards)
-'''
+print(contacts)
