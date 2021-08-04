c102de9f53b63fea5c6e2715469bcc71272a6134
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-08-03 18:34:09 -0700

    updates

--- code/patrick/Python/mini_cap.py
+++ code/patrick/Python/mini_cap.py
@@ -1,28 +1,10 @@
-import requests
-# from requests import api #https://realpython.com/python-requests/#getting-started-with-requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
-
-from requests.exceptions import HTTPError 
-for url in ['https://www.sec.gov/edgar/search-and-access']:
-    try:
-        #response.encoding = 'json'
-        
-        response = requests.get(url)
-        # json_response = response.json()
-        # repository = json_response['items'][0]
-        # print(repository)
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print(f'HTTP error occured: {http_err}')
-    except Exception as err:
-        print(f'Other error occurred: {err}')
-    else:
-        print('Success!')
-
-print(response.apparent_encoding)
-# print(response.url)
-
-# api
-
-
-#maybe building this out in Django would be better?
+driver = webdriver.Chrome() #path to chrominium, inside (), is not needed if included in the same folder.
+driver.get("https://instagram.com")
+try:
+    
