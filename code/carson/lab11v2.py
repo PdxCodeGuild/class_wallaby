156e137f43395281ec738f3980425add35335ad6
ccloter <ccloter@gmail.com>
2021-08-04 18:04:22 -0700

    'project

--- code/carson/lab11v2.py
+++ code/carson/lab11v2.py
@@ -2,45 +2,17 @@ nums = [1,2,3,4,5,6,7,8]
 #n = len(nums)
 search = int(input('Enter a number : '))
 
-# def binary_search(list, object, n):
-#     low = 0
-#     high =  n - 1
-#     mid = ((low + high) // 2)
-#     while low <= high:
-#         if object > high or object < low:
-#             return 'Number out of range' 
-#         elif mid == object:
-#             return list.index(mid)
-#         elif object == high:
-#             return list.index(high)
-#         elif object == low:
-#             return list.index(low)
-#         else:
-#             return 'Unable to find index'
-
-# print(f'Input Number: {search} \n Index: {binary_search(nums, search, n)} ')
-
-# '''2,5,6,7 don't work properly'''
-
-
-       
-            
-def binary_search(nums,i):
-    first = 0
-    print('first', first)
-    last = len(nums)-1
-    while  first <= last :
-        mid = (first + last) // 2
-        print('mid', mid)
-        guess = nums[mid]
-        print('guess',guess)
-        if guess == i :
-            return i
-        elif i < guess:
-            last = mid - 1
-            print('now last is ', last)
-        else:
-            first = mid + 1	
-    return None
-
+def binary_search(nums, i):
+  low = 0
+  high = len(nums)
+  while low < high:
+    mid = (low + high) // 2
+    if nums[mid] < i:
+      low = mid + 1
+    elif nums[mid] > i:
+      high = mid - 1
+    else:
+      return mid
+  return 'Unable to find index'
+  
 print(binary_search(nums, search))
--- code/carson/notify.py
+++ code/carson/notify.py
@@ -1,15 +1,15 @@
 
 
-# import requests
+import requests
 
-# response = requests.get('https://www.americanmuscle.com/corsa-mustang-xtreme-cat-back-exhaust-black-tips-21040blk.html')
-# data = response.json()
-# print(data)
+response = requests.get('https://www.americanmuscle.com/corsa-mustang-xtreme-cat-back-exhaust-black-tips-21040blk.html')
+#data = response.json()
+print(response.text)
 
 
 
-import json
-from urllib.request import urlopen
+# import json
+# from urllib.request import urlopen
 
-f = urlopen("https://www.americanmuscle.com/corsa-mustang-xtreme-cat-back-exhaust-black-tips-21040blk.html")
-j = json.load(f)
+# f = urlopen("https://www.americanmuscle.com/corsa-mustang-xtreme-cat-back-exhaust-black-tips-21040blk.html")
+# j = json.load(f)
