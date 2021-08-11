bb8b167dc21e97929d257288d6e967a2a659877f
ojwesley <olana.wesley@outlook.com>
2021-08-10 23:36:18 -0400

    Lab Blog

--- code/olana/Lab17.py
+++ code/olana/Lab17.py
@@ -23,6 +23,7 @@ while True:
     entry_1 = input(f'enter the first word: ')
     entry_2 = input(f'enter the second word: ')
     
+print 
 
     if list(entry_1) == list(entry_2):
         print (f'{entry_1} and {entry_2} are anagrams')
