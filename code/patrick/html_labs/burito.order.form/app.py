d18be9ed0f43fe6a898413362c2c2836f8ca6a67
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-08-23 18:00:05 -0700

    updates

--- code/patrick/html_labs/burito.order.form/app.py
+++ code/patrick/html_labs/burito.order.form/app.py
@@ -10,9 +10,25 @@ def temperature():
     if request.method == 'POST':
         
         email = request.form['email'] # Jane
+        password = request.form['password']
+        check_password = request.form['check_password']
+        address_1 = request.form['address_1']
+        address_2 = request.form['address_2']
+        address_city = request.form['address_city']
+        address_state = request.form['address_state']
+        address_zip = request.form['address_zip']
         # person_age = request.form['person_age'] # 34
-        print("email username", email)
-        return render_template('user-input.html',email=email)
+        print("email username", email, password, check_password)
+        return render_template('user-input.html' ,
+        email=email, 
+        password=password, 
+        check_password=check_password,
+        address_1=address_1,
+        address_2=address_2,
+        address_city=address_city,
+        address_state=address_state,
+        address_zip=address_zip,
+        )
     elif request.method == 'GET':
         return 'a GET request was made.'
 
