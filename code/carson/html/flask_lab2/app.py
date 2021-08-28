e2e0f73eb34031c444408daffd2d8bda7a114b84
ccloter <ccloter@gmail.com>
2021-08-27 21:02:00 -0700

    'flasklab2

--- code/carson/html/flask_lab2/app.py
+++ code/carson/html/flask_lab2/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, redirect, render_template, request
+
 app = Flask(__name__)
 units = { 'feet': 0.3048,
           'miles': 1609.34,
@@ -7,27 +8,37 @@ units = { 'feet': 0.3048,
           'yards': .9144,
           'inches': .0254}
 
+def converter(distance,distance_choice):
+        
+  pool = units[distance_choice]
 
+  distance = int(distance)
+
+  total = distance * pool
+        
+  return total
 
 
 @app.route('/')
-@app.route('/home/', methods=['POST'])
+@app.route('/home/', methods= ['POST','GET'])
+
 def home():
+  if request.method =='POST':
 
-        # distance_choice = input
+    distance = request.form['distance']
+    print(f'Distance: {distance}')
+    distance_choice = request.form['distance_choice'].lower()
+    print(f'Distance Choice: {distance_choice}')
 
-        # pool = units[distance_choice]
-
-        # distance = input
-
-        # distance = int(distance)
-
-        # total = distance * pool
-
-  return render_template('index.html')
-
-
-
-
+    output = converter(distance,distance_choice)
+    print(f'output: {output}')
     
+    return render_template('index.html', output=output, distance=distance,distance_choice=distance_choice  )
+
+  elif request.method == 'GET': 
+    
+    return render_template('index.html')
+      
+
+ 
 app.run(debug=True)
