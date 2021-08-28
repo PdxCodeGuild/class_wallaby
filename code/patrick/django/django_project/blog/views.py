7c9e9c76fe641fa92f2f0fb485c69e6f2f21772a
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-08-28 10:56:29 -0700

    updates

--- code/patrick/django/django_project/blog/views.py
+++ code/patrick/django/django_project/blog/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from .models import Post
 
-posts = [
-    {
-        'author': 'Barry',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 26, 2021',
+# posts = [
+#     {
+#         'author': 'Barry',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 26, 2021',
 
-    },
-    {
-        'author': 'Patrick',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 25, 2021',
-    }
+#     },
+#     {
+#         'author': 'Patrick',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 25, 2021',
+#     }
 
-]
+# ]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
