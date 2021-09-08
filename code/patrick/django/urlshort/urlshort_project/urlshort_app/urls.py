f33efb3fa329f297c992a001d14d869204173d0e
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-09-08 13:55:51 -0700

    views up and running with generic edits

--- code/patrick/django/urlshort/urlshort_project/urlshort_app/urls.py
+++ code/patrick/django/urlshort/urlshort_project/urlshort_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import  home, ShortCreateView, ShortUpdateView, ShortDeleteView
 
 urlpatterns = [
     path('', home, name='home'),
+    path('short/add/', ShortCreateView.as_view(), name='short-add'),
+    path('short/<int:pk>/', ShortUpdateView.as_view(), name='short-update'),
+    path('short/<int:pk>/delete/', ShortDeleteView.as_view(), name='short-delete'),
 
 ]
--- code/patrick/django/urlshort/urlshort_project/urlshort_app/views.py
+++ code/patrick/django/urlshort/urlshort_project/urlshort_app/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render, redirect 
 from .models import Short
-
+from django.views.generic.edit import CreateView, DeleteView, UpdateView 
+from django.urls import reverse_lazy
+# from urlshort_app.forms import UrlForm
+# from django.views.generic import (CreateView, ListView)
 
 def home(request):
-    return render (request, 'urlshort_app/index.html') 
+    return render(request, 'urlshort_app/home.html', {'title': 'About'})
+
+
+class ShortCreateView(CreateView):
+    model = Short
+    fields = ['url']
+
+class ShortUpdateView(UpdateView):
+    model = Short
+    fields = ['__all__']
+
+class ShortDeleteView(DeleteView):
+    model = Short
+    fields = ['__all__']
+
+
+# class URLFormView(FormView):
+#     model = Short
+#     template_name = 'urlshort_app/form.html'
+#     form_class = UrlForm
+    
+
+#     def form
+    
+    
+# class URLFormView(CreateView):
+#     model = Short
+#     fields = ['code', 'url']
+
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         return super().form_valid(form)
+
