377c735302f80eaecb8b819cd206f521878b6dce
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-09-10 08:36:59 -0700

    updates

--- code/patrick/django/urlshort/urlshort_project/urlshort_app/models.py
+++ code/patrick/django/urlshort/urlshort_project/urlshort_app/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 from django.urls import reverse
 class Short(models.Model):
     code = models.CharField(max_length=200)
     url = models.CharField(max_length=300)
+    date = DateTimeField(auto_now_add=True)
+    code = models.CharField(max_length=10, unique=True, blank=True)
+
+    class Meta:
+        ordering = ["-date"]
+
+    def __str__(self):
+        return f'{self.url} to {self.code}'
     
     def get_absolute_url(self):
         return reverse('short-detail', kwargs={'pk': self.pk})
