e114eecbc12798cede718f650741d1ef9af68ce7
alexdemichieli <demichieli.alessandro@gmail.com>
2021-09-15 21:05:03 -0700

    working on authentication and user session

--- Django/Solutions/Django Cart + Rest/cart_project/urls.py
+++ Django/Solutions/Django Cart + Rest/cart_project/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cart_app.urls'))
+    path('', include('cart_app.urls')),
+    path('accounts/', include('accounts.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
