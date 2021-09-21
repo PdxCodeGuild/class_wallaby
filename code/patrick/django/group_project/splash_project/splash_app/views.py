aaa18f138baf28aca33dd67d09fb5bbe99ae17cc
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-09-20 21:36:17 -0700

    updates

--- code/patrick/django/group_project/splash_project/splash_app/views.py
+++ code/patrick/django/group_project/splash_project/splash_app/views.py
@@ -20,45 +20,50 @@ import string
 
 
 def home(request):
-    image = f'https://api.unsplash.com/photos/random/?client_id={key}'
-    response = requests.get(image).json()
-    photographer = response['user']['username']
-    width = response['width']
-    height = response['height']
-    color = response['color']
-    description = response['description']
-    raw = response['urls']['raw']
-    full = response['urls']['full']
-    small = response['urls']['small']
-    regular = response['urls']['regular']
-    download = response['links']['download']
-    thumb = response['urls']['thumb']
-    alt_description = response['alt_description']
-    sku = response['id']
-    data = ImageModel(
-        photographer=photographer,
-        width = width, 
-        height = height, 
-        color = color, 
-        description = description,
-        raw=raw,
-        full=full, 
-        small=small,
-        regular=regular, 
-        download=download, 
-        thumb=thumb, 
-        alt_description=alt_description,
-        sku=sku,
-    )
-    data.save()
-    context = {
-     'thumb' : response['urls']['thumb'],
-     'alt_description': response['alt_description'], 
-     'sku': response['id'], 'data': data
-     }
-    return render(request, 'splash_app/home.html', context = context)
+    
+        search_term = request.POST['query1']
+   
+        image = f'https://api.unsplash.com/search/photos?page=1&per_page&query={search_term}&client_id={key}'
+        response = requests.get(image).json()
+        print(response)
+        sku = response['results'][1]['id']
+        photographer = response['results'][1]['user']['username']
+        width = response['results'][1]['width']
+        height = response['results'][1]['height']
+        color = response['results'][1]['color']
+        description = response['results'][1]['description']
+        raw = response['results'][1]['urls']['raw']
+        full = response['results'][1]['urls']['full']
+        small = response['results'][1]['urls']['small']
+        regular = response['results'][1]['urls']['regular']
+        download = response['results'][1]['links']['download']
+        thumb = response['results'][1]['urls']['thumb']
+        alt_description = response['results'][1]['alt_description']
+        data = ImageModel(
+            photographer=photographer,
+            width = width, 
+            height = height, 
+            color = color, 
+            description = description,
+            raw=raw,
+            full=full, 
+            small=small,
+            regular=regular, 
+            download=download, 
+            thumb=thumb, 
+            alt_description=alt_description,
+            sku=sku,
+        )
+        data.save()
+        context = {
+        'thumb' : response['results'][1]['urls']['thumb'],
+        'alt_description': response['results'][1]['alt_description'], 
+        'sku': response['results'][1]['id'], 'data': data
+        }
+        return render(request, 'splash_app/home.html', context = context)
 
- 
+
+
 
 def image_detail(request, id):
     img_detail = ImageModel.objects.get(id =id)
