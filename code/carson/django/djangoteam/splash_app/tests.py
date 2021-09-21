3b882c5d05ba0568c66a31077263807039bf1b08
ccloter <ccloter@gmail.com>
2021-09-21 10:42:04 -0700

    'updates

--- code/carson/django/djangoteam/splash_app/tests.py
+++ code/carson/django/djangoteam/splash_app/tests.py
@@ -1,5 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-key == ' oipa2QJFQiBFEcO4MMvho2qH_kIrvBl72vM8_y7g538'
-image== f'https://api.unsplash.com/GET/photos/random?client_id={key}'
