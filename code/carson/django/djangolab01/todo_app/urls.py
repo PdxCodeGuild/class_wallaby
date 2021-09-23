189f81f27d714be3123595562a3bb1b02c5a745c
ccloter <ccloter@gmail.com>
2021-09-22 21:24:48 -0700

    'RestTutorial

--- code/carson/django/djangolab01/todo_app/urls.py
+++ code/carson/django/djangolab01/todo_app/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
     path('', views.home, name = 'home'),
@@ -9,5 +15,7 @@ urlpatterns = [
     path('view_all/', views.view_all, name = 'view_all'),
     path('post_details/<int:id>', views.post_details, name = 'details'),
     path('update_post/<int:id>', views.update_post, name = 'update_post'),
-    path('delete_post/<int:id>', views.delete_post, name = 'delete_post')
+    path('delete_post/<int:id>', views.delete_post, name = 'delete_post'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- code/carson/django/djangolab01/todo_app/views.py
+++ code/carson/django/djangolab01/todo_app/views.py
@@ -1,53 +1,80 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from .models import Author, Note
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 def home(request):
     return render(request, 'pages/home.html')
 
+
 def about(request):
     return render(request, 'pages/about.html')
 
-def register_author(request): 
-    if request.method == 'GET': 
-        return render(request, 'pages/register.html')
-    elif request.method == 'POST': 
-        first_name = request.POST['first_name']  
-        last_name = request.POST['last_name']    
-        Author.objects.create(first_name = first_name, last_name = last_name,)
-        return redirect('add_posts')
-def add_note(request):
-    authors = Author.objects.all() 
-    context = {'authors': authors} 
+
+def register_author(request):
     if request.method == 'GET':
-        return render(request, 'pages/add_note.html', context) 
+        return render(request, 'pages/register.html')
+    elif request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        Author.objects.create(first_name=first_name, last_name=last_name,)
+        return redirect('add_posts')
+
+
+def add_note(request):
+    authors = Author.objects.all()
+    context = {'authors': authors}
+    if request.method == 'GET':
+        return render(request, 'pages/add_note.html', context)
     elif request.method == 'POST':
         title = request.POST['title']
         text = request.POST['text']
         pub_date = request.POST['pub_date']
         author = request.POST['author']
-        result = Author.objects.get(id = author)
-        Note.objects.create(author = result, title = title, text = text, pub_date = pub_date)
+        result = Author.objects.get(id=author)
+        Note.objects.create(author=result, title=title,
+                            text=text, pub_date=pub_date)
         return redirect('view_all')
+
+
 def view_all(request):
-  authors = Author.objects.all()
-  return render(request, 'pages/all.html', {'authors': authors}) 
+    authors = Author.objects.all()
+    return render(request, 'pages/all.html', {'authors': authors})
 
-def post_details(request, id): 
-    post = Note.objects.get(id = id) 
-    return render(request, 'pages/post_details.html', {"post": post}) 
 
-def update_post(request,id):
-    if request.method =='GET':
+def post_details(request, id):
+    post = Note.objects.get(id=id)
+    return render(request, 'pages/post_details.html', {"post": post})
+
+
+def update_post(request, id):
+    if request.method == 'GET':
         return render(request, 'pages/post_details.html')
     elif request.method == 'POST':
-        post = Note.objects.get(id = id) 
+        post = Note.objects.get(id=id)
         post.text = request.POST['edit_description']
         post.save()
         return redirect('view_all')
 
-def delete_post(request,id):
-    post = Note.objects.get(id = id)
+
+def delete_post(request, id):
+    post = Note.objects.get(id=id)
     post.delete()
     return redirect('view_all')
-
-
