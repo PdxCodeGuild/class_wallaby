41e7b0e134837628d044fa3f6784366a478f91ee
ojwesley <olana.wesley@outlook.com>
2021-09-28 00:30:50 -0400

    lab1 v1 Javascript Unit Converter

---
+++ code/olana/Java/lab1_java_unit converter_v1.js
@@ -0,0 +1,17 @@
+//setup unit objects
+const unit = {
+  ft: 0.3048,
+  mi: 1609.34,
+  m: 1,
+  km: 1000,
+};
+
+//user input
+const unitInput = prompt("Please enter unit: ");
+const numberInput = prompt("Please enter distance: ");
+
+//converter
+console.log(unitInput == 'ft')
+    alert(.3048 * numberInput);
+
+
