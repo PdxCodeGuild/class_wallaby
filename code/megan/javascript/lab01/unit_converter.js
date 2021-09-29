99a4840e9589e4347d825c044ab185a2e23cf544
meganxcook <cook.me.eliz@gmail.com>
2021-09-28 21:38:13 -0700

    JS lab01 progress

--- code/megan/javascript/lab01/unit_converter.js
+++ code/megan/javascript/lab01/unit_converter.js
@@ -1,26 +1,39 @@
 // Version 1
 
-const distance = prompt('What is the distance in feet?')
-
-// let conversion = `${distance} feet is ${...}`
-alert('conversion')
-console.log(conversion)
+// const distance = prompt('What is the distance in feet?')
+// alert(`${distance} feet is ${(distance * 0.3048)} meters.`)
+// console.log(`${distance} feet is ${(distance * 0.3048)} meters.`)
 
 // Version 2
 
-const distance = prompt('What is the distance?')
-const units = prompt('What are the units (ft, mi, m, or km)?')
+// const distance = prompt('What is the distance?')
+// console.log(distance)
 
-conversion_dict = {
-    'ft': 0.3048,
-    'mi': 1609.34,
-    'm': 1,
-    'km': 1000,
-}
+// const units = prompt('What are the units (ft, mi, km)?')
+// console.log(units)
+
+// const conversionFactor = {
+//     'ft': 0.3048,
+//     'mi': 1609.34,
+//     'm': 1,
+//     'km': 1000,
+// }
+
+// alert(`${distance} ${units} is ${(distance * conversionFactor[units])} meters`)
+// console.log(`${distance} ${units} is ${(distance * conversionFactor[units])} meters`)
 
 // Version 3
 
-const units = prompt('What are the units (ft, mi, m, km, yd, or in)?')
+// conversionFactor.add { // not correct; fix
+//     'yd': 0.9144,
+//     'in': 0.0254,
+// }
+
+const distance = prompt('What is the distance?')
+console.log(distance)
+
+const units = prompt('What are the units (ft, mi, km, yd, in)?')
+console.log(units)
 
 const conversionFactor = {
     'ft': 0.3048,
@@ -30,5 +43,9 @@ const conversionFactor = {
     'yd': 0.9144,
     'in': 0.0254
 }
+
+alert(`${distance} ${units} is ${(distance * conversionFactor[units])} meters`)
+console.log(`${distance} ${units} is ${(distance * conversionFactor[units])} meters`)
+
 
 // Version 4
