ee03b1fd60541fe054d7a5e12040af32c4f830e9
meganxcook <cook.me.eliz@gmail.com>
2021-09-30 21:20:49 -0700

    JS lab 01 progress

--- code/megan/javascript/lab01/pick6.js
+++ code/megan/javascript/lab01/pick6.js
@@ -1,14 +1,41 @@
 function pick6() {
-    const ticket = []
-    for (let i = 0; i < 6; i++)
-        ticket.push(Math.floor(Math.random() * )) // max & min?
+
+    let ticket = [];
+    let min = 1;
+    let max = 99;
+
+    function getRandomInt(min, max) {
+        // let min = Math.ceil(1);
+        // let max = Math.floor(99);
+        console.log(Math.floor(Math.random() * (max - min)))
+        return Math.floor(Math.random() * (max - min))
+    };
+
+    for (let i = 0; i < 7; i++) {
+        ticket.push(getRandomInt(min, max));
+    }
+
+    return ticket
 }
 
-function compareMatches(winner, ticket) {
+const winningTicket = pick6()
+console.log(winningTicket)
 
+function compareMatches(winning, ticket) {
+    console.log(winning, ticket)
+
+    let matches = 0;
+
+    for (let i = 0; i < winning.length; i++) {
+        if (winning[i] === ticket[i])
+            matches += 1;
+    }
+
+    return matches
 }
 
 function winningsFunction() {
+
     const winningsObject = {
         1: 4,
         2: 7,
@@ -18,21 +45,38 @@ function winningsFunction() {
         6: 25000000
     }
 
-    let earnings = 0
-    let expenses = 0
+    let balance = 0;
+    let expenses = 0;
+    let earnings = 0;
+    let roi = 0;
 
-    while () {
+    // let indicators = {
+    //     balance: 0,
+    //     expenses: 0,
+    //     earnings: 0,
+    //     roi: 0,
+    // }
+
+    for (let i = 0; i < 3; i++) {
+        balance -= 2;
         expenses += 2;
-        earnings += winningsObject[];
-
+        earnings += (winningsObject[compareMatches(winningTicket, pick6())]);
+        roi += ((earnings - expenses) / expenses);
     }
 
-    let balance = earnings - expenses
-    let roi = earnings - expenses / expenses
+    // let roi = earnings - expenses / expenses;
+
+    let indicators = [
+        balance[i],
+        expenses[i],
+        earnings[i],
+        roi[i],
+    ]
 
     console.log(`After 100,000 tries, your balance is $${balance}. \nEarnings: $${earnings} \nExpenses: $${expenses} \nReturn on investment(ROI): $${roi}`)
     alert(`After 100,000 tries, your balance is $${balance}. \nEarnings: $${earnings} \nExpenses: $${expenses} \nReturn on investment(ROI): $${roi}`)
 
+    return indicators
 }
 
 winningsFunction()
--- code/megan/javascript/lab01/rot13.js
+++ code/megan/javascript/lab01/rot13.js
@@ -1,19 +1,24 @@
-const input = prompt('Enter a secret message:')
+const input = prompt('Enter a secret message:');
 
-let alphabetLower = 'abcdefghijklmnopqrstuvwxyz'
-let alphabetUpper = alphabetLower.toUpperCase()
+let alphabetLower = 'abcdefghijklmnopqrstuvwxyz';
+let alphabetUpper = alphabetLower.toUpperCase();
+// how to account for uppercase, lowercase, and spaces?
 
-let output = []
+let output = [];
 
 for (i = 0; i < input.length; i++) {
     letter = alphabetLower.indexOf(input[i]);
-    let x = output.push(alphabetLower[(letter + 13) % 26]); // fix; skipping
-    console.log(output)
-    i++;
+    output.push(alphabetLower[(letter + 13) % 26]);
+    // output.join(',');
+    console.log(output);
 }
+
+// convert array into string
+// console.log(output.join(','))
+
 
 alert(`Original message: ${input} \nEncrypted message: ${output}`)
 console.log(`Original message: ${input} \nEncrypted message: ${output}`)
 
 // test:
-// help me = uryc zr
+// hello = uryyb
