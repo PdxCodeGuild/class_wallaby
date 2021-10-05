b5e966d844d3ff542139b8e1bc6445ac9a00bcbf
ChrisHolley <asiholley@gmail.com>
2021-10-04 21:23:36 -0700

    add delete functionality

--- code/chris/javaScript/lab02/app.js
+++ code/chris/javaScript/lab02/app.js
@@ -6,8 +6,20 @@ const compLi = document.getElementById('completeList')
 
 const addTodo = e => {
   const newTodo = document.createElement('li')
+  newTodo.className = 'todo'
   newTodo.innerText = input.value
+  input.value = ''
   incompLi.appendChild(newTodo)
+}
+
+const deleteTodo = e => {
+  const todosArray = document.getElementsByClassName('todo')
+  for (const todo of todosArray) {
+    if (todo.innerText === input.value){
+      todo.parentNode.removeChild(todo)
+    }
+  }
+  input.value = ''
 }
 
 const markComplete = e => {
@@ -18,3 +30,4 @@ const markComplete = e => {
 
 addBtn.addEventListener('click', addTodo)
 incompLi.addEventListener('click', markComplete)
+deleteBtn.addEventListener('click', deleteTodo)
