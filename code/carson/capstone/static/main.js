93e2289dc21f0ad106acdc964a2dbd90e6ee2e55
ccloter <ccloter@gmail.com>
2021-10-12 21:41:11 -0700

    'Capstone

---
+++ code/carson/capstone/static/main.js
@@ -0,0 +1,26 @@
+console.log('hello worl');
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        //Toggle NAv   
+        nav.classList.toggle('nav-active');
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s `;
+                console.log(index / 7)
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlide();
