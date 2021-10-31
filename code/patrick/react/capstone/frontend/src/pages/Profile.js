9f27d7f827f365de0ad25b20bd089a2e9ed214c3
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-10-31 14:15:03 -0700

    modal with update profile picture option working

--- code/patrick/react/capstone/frontend/src/pages/Profile.js
+++ code/patrick/react/capstone/frontend/src/pages/Profile.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Card, Col, Container, Form, Modal, Row } from "react-bootstrap";
 import "./Profile.css";
 import { useForm } from "react-hook-form";
+import { CardBody } from "reactstrap";
+import Button from "@restart/ui/esm/Button";
 
 function Profile() {
   const { register, handleSubmit, reset } = useForm();
   const [image, setImage] = useState(null);
   const [newImage, setNewImage] = useState(null);
   const [userID, setUserID] = useState("");
-  // const [userName, setUserName] = useState("")
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const ImageChange = (e) => {
     setNewImage(e.target.files[0]);
@@ -24,20 +29,17 @@ function Profile() {
       .patch("http://localhost:8000/imageupload/", form_data)
       .then((res) => {
         setImage(res.data.image);
+        handleClose()
       })
       .catch((err) => console.log(err));
   };
 
   async function updateProfile(data) {
-    console.log(data, ' user id');
+    console.log(data, " user id");
     // feedsubs/pk which whole feeds user is subscribed to
     await axios
       .patch("http://localhost:8000/edituser/" + userID, data)
       .then((res) => {
-        //   setUserName(res.data.username)
-        //   setEmail(res.data.email)
-        //   setFirstName(res.data.first_name)
-        //   setLastName(res.data.last_name)
         console.log(res);
       });
   }
@@ -45,8 +47,6 @@ function Profile() {
     axios.get("http://localhost:8000/dj-rest-auth/user/").then((res) => {
       setUserID(res.data.pk);
       setImage(res.data.profile["image"]);
-      
-     
       setTimeout(() => {
         reset({
           username: res.data.username,
@@ -60,39 +60,66 @@ function Profile() {
 
   return (
     <Container>
-      <div>
-        <form onSubmit={Submit}>
-          {/* <p>
-            <input type="text" placeholder='Title' id='title' value={this.state.title} onChange={this.handleChange} required/>
-          </p>
-          <p>
-            <input type="text" placeholder='Content' id='content' value={this.state.content} onChange={this.handleChange} required/>
+      <Row>
+        <Col md={4}>
+          <Card >
+            <CardBody>
+              <Card.Img variant="top" img idi="profileImage" src={image} alt="ima" />
+              
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      <Modal show={show} autoFocus="true" onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form.Group className="mb-3">
+                <form onSubmit={Submit}>
+                  <p>
+                    <input
+                      type="file"
+                      id="image"
+                      accept="image/png, image/jpeg"
+                      onChange={ImageChange}
+                      required
+                    />
+                  </p>
+                </form>
+                  {/* <Button
+                    value="Upload Photo"
+                    className="btn btn-primary"
+                    type="submit"
+                    onChange={(e) => setNewImage( e.target.value)}
+                  /> */}
+              </Form.Group>
 
-          </p> */}
-          <p>
-            <input
-              type="file"
-              id="image"
-              accept="image/png, image/jpeg"
-              onChange={ImageChange}
-              required
-            />
-          </p>
-          <input type="submit" />
+
+        </Modal.Body>
+        <Modal.Footer>
+       
+          <Button className="btn btn-primary" variant="primary" onClick={Submit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+        <form onSubmit={handleSubmit(updateProfile)}>
+          <label>Email: </label>
+          <input {...register("email")} />
+          <label>Username: </label>
+          <input {...register("username")} />
+          <label>First name: </label>
+          <input {...register("first_name")} />
+          <label>Last name: </label>
+          <input placeholder="last name" {...register("last_name")} />
+          <input value="Update" className="btn btn-primary" type="submit" />
         </form>
-      </div>
-      <img src={image} alt="ima" />
-      <form onSubmit={handleSubmit(updateProfile)}>
-        <label>Email: </label>
-        <input {...register("email")} />
-        <label>Username: </label>
-        <input {...register("username")} />
-        <label>First name: </label>
-        <input {...register("first_name")} />
-        <label>Last name: </label>
-        <input {...register("last_name")} />
-        <input type="submit" />
-      </form>
+      </Form.Group>
     </Container>
   );
 }
