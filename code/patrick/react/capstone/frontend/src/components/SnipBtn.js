7b3a0d53117524949f6c1b8e0c9b328cb400133a
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-11-02 15:31:28 -0700

    updates

--- code/patrick/react/capstone/frontend/src/components/SnipBtn.js
+++ code/patrick/react/capstone/frontend/src/components/SnipBtn.js
@@ -4,51 +4,42 @@ import React, { useEffect, useState } from "react";
 export default function SnipBtn(id) {
   const [snipDetail, setSnipDetail] = useState(id.id.subscriber);
   const [snipbtn, setSnipBtn] = useState("add");
-  const [userID, setUserID] = useState(parseInt(localStorage["UserID"]))
-  
+  const [userID, setUserID] = useState(parseInt(localStorage["UserID"]));
+
   useEffect(() => {
-    UpdateSubs();
+    console.log(snipDetail, "UseEffect details");
   }, [snipDetail]);
-  
+
   function UpdateSubs() {
     const data = {
       subscriber: snipDetail,
     };
     console.log(data, " request details sent");
-    axios
+     axios
       .patch(`http://localhost:8000/snipsubs/${id.id.id}`, data)
       .then((res) => {
         console.log(res.data.subscriber, " return patch request");
       });
   }
 
-
   function toAdd() {
     if (snipDetail.includes(userID) !== true) {
-      console.log("is not subscribed test");
+      setSnipDetail([...snipDetail, userID]);
+      setSnipBtn("remove");
+      UpdateSubs();
     }
-    setSnipDetail((snipDetail) => [...snipDetail, userID]);
-    setSnipBtn("remove");
-    UpdateSubs();
-    console.log(snipDetail)
   }
 
   function toRemove() {
+    if (snipDetail.includes(userID) === true) {
     let index = snipDetail.indexOf(userID);
     snipDetail.splice(index, 1);
-    console.log(snipDetail, " remove button output");
-    var x = [];
-    for (let i = 0; i < snipDetail.length; i++) {
-      if (snipDetail[i] !== 1) {
-        x.push(snipDetail[i]);
-        console.log(snipDetail[i], " inside the for loop");
-      }
-    }
-    console.log(x, " for loop");
-    setSnipDetail([x]);
+    // setSnipDetail(snipDetail);
     setSnipBtn("add");
     UpdateSubs();
+    console.log(snipDetail, " remove button output");
   }
+}
 
   return (
     <div className={snipbtn}>
@@ -64,3 +55,9 @@ export default function SnipBtn(id) {
     </div>
   );
 }
+
+// for (let i = 0; i < snipDetail.length; i++) {
+//   if (snipDetail[i] !== 1) {
+//     x.push(snipDetail[i]);
+//     console.log(snipDetail[i], " inside the for loop");
+//   }
